29aa0ae443bb50675819f9131a56aa911a802a44
WonJun Yeo <rbgh004@gmail.com>
2022-05-01 23:55:48 +0900

    2022.05.01

---
+++ src/bronze/ascii/Main.java
@@ -0,0 +1,10 @@
+package bronze.ascii;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char a = sc.next().charAt(0);
+		System.out.println((int)a);
+		sc.close();
+	}
+}
