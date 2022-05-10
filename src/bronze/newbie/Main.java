6992bccc34e2780f9c2a3e7ad56091f1e343aac6
WonJun Yeo <rbgh004@gmail.com>
2022-05-10 20:13:09 +0900

    2022.05.10

---
+++ src/bronze/newbie/Main.java
@@ -0,0 +1,24 @@
+package bronze.newbie;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if (b == 1 || b == 2) {
+			System.out.println("NEWBIE!");
+		} else if (b <= a) {
+			System.out.println("OLDBIE!");
+		} else {
+			System.out.println("TLE!");
+		}
+		
+		
+		sc.close();
+	}
+
+}
