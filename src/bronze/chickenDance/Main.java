4eab5b4c70d50de4fcec2dd9d34bed6849b389ad
WonJun Yeo <rbgh004@gmail.com>
2022-05-18 21:41:51 +0900

    2022.05.18

---
+++ src/bronze/chickenDance/Main.java
@@ -0,0 +1,23 @@
+package bronze.chickenDance;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int d = b / 2;
+		
+		int e = d + c;
+		if (e > a) {
+			System.out.println(a);
+		} else {
+			System.out.println(e);
+		}
+		sc.close();
+	}
+}
