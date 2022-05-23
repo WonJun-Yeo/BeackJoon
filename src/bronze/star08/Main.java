69df7c2724b1a838f125b8c8a1a7042762432f19
WonJun Yeo <rbgh004@gmail.com>
2022-05-23 18:29:53 +0900

    2022.05.23

---
+++ src/bronze/star08/Main.java
@@ -0,0 +1,36 @@
+package bronze.star08;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= 2 * (a - i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 1; i < a; i++) {
+			for (int j = 1; j <= a - i; j++) {
+				System.out.print("*");
+			}
+			for (int j =1; j <= 2 * i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= a - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
