c446573855629b5285108df6841022fdcc1e46e0
WonJun Yeo <rbgh004@gmail.com>
2022-05-25 20:25:54 +0900

    2022.05.25

---
+++ src/bronze/star12/Main.java
@@ -0,0 +1,27 @@
+package bronze.star12;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= a - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = a-1; i <= a && i > 0; i--) {
+			for (int j = 1; j <= a - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
