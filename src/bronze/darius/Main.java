120687d59cf091e1ee124b44ab85e6d0f6f695f8
WonJun Yeo <rbgh004@gmail.com>
2022-05-28 14:37:04 +0900

    2022.05.28

---
+++ src/bronze/darius/Main.java
@@ -0,0 +1,25 @@
+package bronze.darius;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		String[] arr = input.split("/");
+		int k = Integer.parseInt(arr[0]);
+		int d = Integer.parseInt(arr[1]);
+		int a = Integer.parseInt(arr[2]);
+		
+		if (k + a < d || d == 0) {
+			System.out.println("hasu");
+		} else {
+			System.out.println("gosu");
+		}
+		
+		
+		sc.close();
+	}
+
+}
